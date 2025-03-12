5705c71a135fcf47047b9dadea71a9fc08ea0c1e
johannes-code <johannes24sfj@gmail.com>
2025-03-12 14:38:41 +0100

    sa

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-fetch("/api/hello").then((req) => console.log(req.text));
+fetch("/api/hello").then((req) => console.log(req.text()));
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
