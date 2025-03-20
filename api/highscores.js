9ec7d8cce650e3300a863e3d1614bc30b9639682
johannes-code <johannes24sfj@gmail.com>
2025-03-20 13:36:15 +0100

    t*

--- api/highscores.js
+++ api/highscores.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import serverless from "serverless-http";
 
-dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
