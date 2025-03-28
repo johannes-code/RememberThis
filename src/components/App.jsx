43f8703b31e8872f7531cf704e160944a6902f2b
scrapjo <darklord85@gmail.com>
2025-03-28 22:48:06 +0100

    changed back

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { CategorySelector } from "./CategorySelector";
 import { GameControls } from "./GameControls";
 import { CardCounter } from "./CardCounter";
@@ -13,32 +13,13 @@ import { useGameStore, useHighscoreStore } from "../stores/index.jsx";
 import NameInput from "./NameInput.jsx";
 
 export default function App() {
-  const { highscores, fetchHighscores } = useHighscoreStore();
-  const { isGameOn, hasGameEnded, count, startGame } =
+  const { isGameOn, hasGameEnded, count, startGame, fetchHighscores } =
     useGameStore();
-  const { setIsLoading} = useState(true);
-  
+  const { highscores } = useHighscoreStore();
 
   useEffect(() => {
-    let isMounted = true;
-    const loadHighscores = async () => {
-      try {
-        setIsLoading(true);
-        await fetchHighscores();
-        if (isMounted) setIsLoading(false);
-      } catch (error) {
-        if (isMounted) {
-          console.error("Loading failed:", error);
-          setIsLoading(false);  // Add this line
-        }
-      }
-    };
-  
-    loadHighscores();
-    return () => { isMounted = false };
+    fetchHighscores;
   }, [fetchHighscores]);
-  
-
 
   return (
     <main>
